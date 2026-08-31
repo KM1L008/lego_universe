37c5041ead64d0ab0dcdcfb4ee909669fde5f6d4
kamscript <camicgc06@gmail.com>
2026-08-30 21:08:45 -0500

    Modificacion de los archivos TargetaLego e Index añadiendo estilos personalizados para las targetas

--- components/TarjetaLego.tsx
+++ components/TarjetaLego.tsx
@@ -1,17 +1,54 @@
 // Importar view y text de react-native
-import { View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { ThemedText } from './themed-text';
+import { ThemedView } from './themed-view';
 
 type TarjetaLegoProps = {
-  nombre: string;
-  piezas: number;
-  disponible: boolean;
+    nombre: string;
+    piezas: number;
+    disponible: boolean;
 };
 
 export default function TarjetaLego({ nombre, piezas, disponible }: TarjetaLegoProps) {
     return (
-        <View>
-            <ThemedText type="defaultSemiBold">Nombre: {nombre}, Piezas: {piezas}, Disponible: {disponible ? 'Disponible' : 'Agotado'}</ThemedText>
-        </View>
+        <ThemedView style={estilos.tarjeta}>
+            {disponible ? (
+                <ThemedText type="defaultSemiBold" style={estilos.disponible}>Disponible</ThemedText>
+            ) : (
+                <ThemedText type="defaultSemiBold" style={estilos.agotado}>Agotado</ThemedText>
+            )}
+            <ThemedText type="defaultSemiBold">Nombre: {nombre}</ThemedText>
+            <ThemedText type="default">Piezas: {piezas}</ThemedText>
+        </ThemedView>
     );
 }
+
+const estilos = StyleSheet.create({
+    tarjeta: {
+        backgroundColor: '#23262b',
+        borderWidth: 1,
+        borderRadius: 8,
+        padding: 16,
+        marginBottom: 16,
+    },
+    disponible: {
+        backgroundColor: '#224131',
+        color: '#91b7a2',
+        padding: 1,
+        paddingLeft: 8,
+        paddingRight: 8,
+        alignSelf: 'flex-start',
+        fontSize: 12,
+        borderRadius: 10,
+    },
+    agotado: {
+        backgroundColor: '#361715',
+        color: '#dcb4b5',
+        padding: 1,
+        paddingLeft: 8,
+        paddingRight: 8,
+        alignSelf: 'flex-start',
+        fontSize: 12,
+        borderRadius: 10,
+    }
+});
